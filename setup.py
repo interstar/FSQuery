4ecdfc2b34de18d1897fa411c926c5e5dafb268b
Phil Jones <interstar@gmail.com>
2018-04-12 20:17:44 -0300

    more in setup

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(name='fsquery',
       version='0.1.6',
-      description='File System Query. The file system is too verbose. Let\'s make it more like JQuery etc.',
+      description='File System Query. Working with the file system is too verbose. Let\'s make it more like JQuery.',
       url='https://github.com/interstar/FSQuery',
       author='Phil Jones',
       author_email='interstar@gmail.com',
       license='MIT',
       packages=['fsquery'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers'],
       zip_safe=False)
