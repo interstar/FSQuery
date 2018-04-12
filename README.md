f6b027cbd5673aef65011e4df90a2291993cbf64
Phil Jones <interstar@gmail.com>
2018-04-12 17:20:49 -0300

    added documentation, etension match, contains (search within file), opening and writing files

---
+++ README.md
@@ -0,0 +1,88 @@
+
+## FSQuery (File System Query)
+
+Working with the file system in Python isn't exactly difficult.
+
+But it *is* verbose. And a low level way of talking to a complex tree of directories and files of different types.
+
+FSQuery is a tiny Python library inspired by JQuery designed to let you query and manipulate a tree in the file system the way JQuery lets you query and manipulate the DOM.
+
+### Quickstart
+
+
+### Things you can do with FSQuery 
+
+Create a query starting at a path and list it
+
+        from fsquery import FSQuery
+
+        fsq = FSQuery(path)
+        for n in fsq :
+            print n
+
+
+Find *files* that match a particular criteria
+
+        fsq = FSQuery(path).Match(".js$")
+        for n in fsq :
+            print n
+
+
+Note that FSQuery matches using an ordinary regex, so you can use regex elements in it. But be careful not to put bad regex in, it will crash.
+
+Add a FileOnly() call, to constrain the output to show only files (not directories)
+
+        fsq = FSQuery("../..").Match(".js$").FileOnly()  
+        for n in fsq :
+            print n
+
+You can also explicitly match just file extensions.
+
+        fsq = FSQuery("../..").Ext("css").FileOnly()  
+        for n in fsq :
+            print n
+
+Filter out a directory you aren't interested in, using NoFollow.
+
+        fsq = FSQuery("../..").Match(".js$").NoFollow("vendor").FileOnly()  
+        for n in fsq :
+            print n
+
+
+Note that you can add as many NoFollows and Matches as you like. NoFollows are patterns that are only applied to directories. Matches are patterns that are only applied to files. Note also that, although you have added a FileOnly() to this query this doesn't affect the pattern matches on the directory names. 
+
+Note also that if you add two Ext() constraints these will fight (no file can have two extensions at the same time) and your query will return nothing.
+
+
+### FSNodes
+
+From the previous examples, you'll notice that what is returned by FSQuery are FSNodes. FSNodes provide several further useful attributes and methods.
+
+`FSNode.abs` is the absolute name of the file or node. 
+
+Eg.
+
+        fsq = FSQuery("../..").Match(".js$").NoFollow("vendor").FileOnly()  
+        for n in fsq :
+            print n.abs
+            
+will lists the returned nodes as ordinary text (as if running `find` in Unix)
+
+`FSNode.open_file` will, if the FSNode is a file, open it for reading and return a file handle. It throws an exception if the FSNode is a directory.
+
+Eg.
+
+        fsq = FSQuery("../..").NoFollow("vendor").Ext("py").FileOnly()
+        for n in fsq :
+            print n.abs
+            for line in n.open_file() :
+                print line.rstrip()
+            print "______________"
+     
+
+
+
+`FSNode.children()` will return the immediate children of the node.
+
+`FSNode.spawn_query()` creates a new FSQuery, starting at this FSNode. 
+
--- fsquery/fsquery.py
+++ fsquery/fsquery.py
@@ -7,24 +7,49 @@ class FSNode :
         self.depth = depth
 
     def dirname(self) :
+        "Path to this node"
         return os.path.dirname(self.abs)
     
     def basename(self) :
+        "Filename or directory name"
         return os.path.basename(self.abs)
+        
+    def ext(self) : 
+        "File extension, where separated by a . Returns an empty string if the file has no name"
+        if not "." in self.basename() : return ""
+        return self.basename().split(".")[-1]
 
     def isdir(self) :
+        "True if this FSNode is a directory"
         return os.path.isdir(self.abs)
 
     def children(self) :
+        "If the FSNode is a directory, returns a list of the children"
+        if not self.isdir() : raise "FSQuery tried to return the children of a node which is not a directory : %s" % self.abs
         return [FSNode(self.abs + "/" + x,self.depth+1) for x in os.listdir(self.abs)]
         
     def spawn_query(self) :
+        """Create a new FSQuery, starting from this node"""
         return FSQuery(self.abs)
         
-    def add_file(self) :
-        if self.isdir() : raise "FSQuery tried to add a file in a node which is not a directory : %s" % self.abs
-        with open(fsNode.abs + "/new2.txt","w") as f :
-            f.write("this is new")
+    def write_file(self,fName,content) :
+        """If this FSNode is a directory, write a file called fName containing content inside it"""
+        if not self.isdir() : raise "FSQuery tried to add a file in a node which is not a directory : %s" % self.abs
+        with open("%s/%s"%(self.abs,fName),"w") as f :
+            f.write(content)
+    
+    def open_file(self) :
+        """If this FSNode is a file, open it for reading and return the file handle"""
+        if self.isdir() : raise "FSQuery tried to open a directory as a file : %s" % self.abse        
+        return open(self.abs)
+        
+    def contains(self,pat) :
+        r = re.compile(pat)
+        with self.open_file() as f :
+            for line in f :
+                if r.search(line) :
+                    return True
+        return False
         
     def __str__(self) :
         return "[FSNode : %s (isdir? %s)]"% (self.abs, self.isdir())
@@ -53,14 +78,19 @@ class FSQuery :
         return self.all(fsNode,self.return_criteria)
 
     def make_file_include(self,f) :
+        "Add the function f as a test for including the FSNode representing a file in the query results."
         self.file_includes.append(f)
         return self
         
     def make_dir_include(self,f) :
+        """Add the function f as a test for including the FSNode representing a directory both during the query and in showing its results.
+
+Directories excluded by this filter are not searched."""
         self.dir_includes.append(f)
         return self
     
     def make_return(self,f) :
+        "Add the function f as a test for showing the FSNode in the final results. This has no effect "
         self.return_criteria.append(f)
         return self
         
@@ -104,6 +134,8 @@ class FSQuery :
         q.return_criteria = self.return_criteria[:]
         return q
         
+    # From here on are the normal methods you'd expect a user to call.
+    
     def Match(self,pat) :
         r = re.compile(pat)
         def match(fsNode) :        
@@ -111,6 +143,12 @@ class FSQuery :
         self.make_file_include(match)
         return self
     
+    def Ext(self,ext) :
+        return self.make_file_include(lambda n : n.ext() == ext)
+
+    def Contains(self,pat) :
+        return self.make_file_include(lambda n : n.contains(pat))
+        
     def NoFollow(self,pat) :
         r = re.compile(pat)
         def match(fsNode) :
@@ -120,6 +158,15 @@ class FSQuery :
             return not r.search(bn)
         self.make_dir_include(match)
         return self
+        
+    def DirContains(self,f) :
+        """ Matches dirs that have a child that matches filter f"""
+        def match(fsNode) :
+            if not fsNode.isdir() : return False 
+            for c in fsNode.children() :
+                if f(c) : return True
+            return False
+        return self.make_return(match)
 
     def FileOnly(self) :
         def match(fsNode) :
@@ -140,15 +187,39 @@ if __name__ == '__main__' :
     class Processor :
         def process_dir(self,fsNode) :
             print("this is a dir : %s" % fsNode)
-            with open(fsNode.abs + "/new2.txt","w") as f :
-                f.write("this is new")
+            fsNode.write_file("new3.txt","This has been made properly")
         def process_file(self,fsNode) :
             pass
             
     fsq.NoFollow("b")        
     fsq.process_each(Processor())
 
-    fsq = FSQuery("../..").Match(".js$").FileOnly()  
+    print "__________________________________________________------------------------------------------------------------"
+    
+
+    fsq = FSQuery("../..").NoFollow("vendor").FileOnly().Ext("css")
     for n in fsq :
-        print n
+        print n.abs, n.ext()
         
+    print "----------------------------------------_______________________________---------------------------------------"
+    
+
+        
+    fsq = FSQuery("../..").NoFollow("vendor").DirContains(lambda n : n.ext() == "py")
+    for n in fsq :
+        print n.abs
+        if n.isdir() :
+            print ["%s"%c.basename() for c in n.children()]
+            
+
+    print "----------------------------------------_______________________________---------------------------------------"
+    fsq = FSQuery("../..").NoFollow("vendor").Ext("py").Contains("GNU Lesser General Public License").FileOnly()
+    for n in fsq :
+        print n.abs
+        #for line in n.open_file() :
+        #    print line.rstrip()
+        #print "_________________________________________________________________________________________"
+    
+    
+
+    
